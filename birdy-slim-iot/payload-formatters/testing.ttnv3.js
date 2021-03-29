68634fca024bd5d35357110496da0264200760fd
cat6e <63829136+catSIXe@users.noreply.github.com>
2021-03-29 19:13:59 +0200

    Update testing.ttnv3.js

--- birdy-slim-iot/payload-formatters/testing.ttnv3.js
+++ birdy-slim-iot/payload-formatters/testing.ttnv3.js
@@ -1,9 +1,1 @@
-function decodeUplink(input) {
-  return {
-    data: Decoder(input.bytes, input.fPort),
-    warnings: [],
-    errors: []
-  };
-}
-
-/** Attach the Code of testing.ttnv2.js **/
+/** Use the Code of testing.ttnv2.js **/

18cc0c22349088248691587975863dd97279cf2a
cat6e <63829136+catSIXe@users.noreply.github.com>
2021-04-03 19:34:13 +0200

    Update gpsonly.js

--- birdy-slim-iot/payload-formatters/gpsonly.js
+++ birdy-slim-iot/payload-formatters/gpsonly.js
@@ -1,10 +1,10 @@
 // TTN V2
 function Decoder(bytes, port) {
   var decoded = {};
-  decoded.latitude = ((bytes[0]<<24)>>>0) + ((bytes[1]<<16)>>>0) + ((bytes[2]<<8)>>>0) + bytes[3];
-  decoded.latitude /= 10e4;
-  decoded.longitude = ((bytes[4]<<24)>>>0) + ((bytes[5]<<16)>>>0) + ((bytes[6]<<8)>>>0) + bytes[7];
+  decoded.longitude = ((bytes[0]<<24)>>>0) + ((bytes[1]<<16)>>>0) + ((bytes[2]<<8)>>>0) + bytes[3];
   decoded.longitude /= 10e4;
+  decoded.latitude = ((bytes[4]<<24)>>>0) + ((bytes[5]<<16)>>>0) + ((bytes[6]<<8)>>>0) + bytes[7];
+  decoded.latitude /= 10e4;
   decoded.lastGPSAcquisition = bytes[8];
   return decoded;
 }
@@ -12,10 +12,10 @@ function Decoder(bytes, port) {
 // TTN V3
 function decodeUplink(input) {
   const bytes = input.bytes, data = {}
-  data.latitude = ((bytes[0]<<24)>>>0) + ((bytes[1]<<16)>>>0) + ((bytes[2]<<8)>>>0) + bytes[3];
-  data.latitude /= 10e4;
-  data.longitude = ((bytes[4]<<24)>>>0) + ((bytes[5]<<16)>>>0) + ((bytes[6]<<8)>>>0) + bytes[7];
+  data.longitude = ((bytes[0]<<24)>>>0) + ((bytes[1]<<16)>>>0) + ((bytes[2]<<8)>>>0) + bytes[3];
   data.longitude /= 10e4;
+  data.latitude = ((bytes[4]<<24)>>>0) + ((bytes[5]<<16)>>>0) + ((bytes[6]<<8)>>>0) + bytes[7];
+  data.latitude /= 10e4;
   data.lastGPSAcquisition = bytes[8];
   return {
     data,
